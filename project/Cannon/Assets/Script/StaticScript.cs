d35b747366f7e25e5a5f9eb22ae80cd19682d059
supermiu <jeffersonlee2000@gmail.com>
2015-03-05 21:28:00 -0800

    project
    stuff

---
+++ project/Cannon/Assets/Script/StaticScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticScript : MonoBehaviour {
+
+	Vector3 originalPos;
+	// Use this for initialization
+	void Start () {
+		originalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = originalPos;
+	}
+}
